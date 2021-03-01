87d3a0be97130e04eff504d68993280d783ca2da
kelly <kelly@zipcodes-MacBook-Pro-9.local>
2021-03-01 08:21:08 -0500

    Completed Alice and Bob exercise

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -10,13 +10,17 @@ public class AliceAndBobEngine
     String result;
     
     AliceAndBobEngine(){
-        result = "";
+            result = "";
     }
 
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
     public String evaluateAliceOrBob(String input)
     {
-    
+        if (input == "Alice" || input == "Bob") {
+            result = "Hello " + input;
+        } else {
+            result = "I DON'T KNOW YOU, YOU ARE SCARY";
+        }
         return result;
     }
 }
